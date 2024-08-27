41b9b34c2516742f24ec12538ba0a214d943c1de
YogaLee12 <s631693747@126.com>
2024-08-27 21:43:07 +1000

    totally completed

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -124,16 +124,16 @@ export async function deleteInvoice(id:string) {
 export async function authenticate(
     prevState: string | undefined,
     formData: FormData,
-){
-    try {
-        await signIn('credentials' , formData);
-    } catch (error) {
+    ) {
+        try {
+        await signIn('credentials', formData);
+        } catch (error) {
         if (error instanceof AuthError) {
-            switch (error.type){
-                case 'CredentialsSignin':
-                    return 'Invalid credentials';
-                default: 
-                    return 'Something went wrong';
+            switch (error.type) {
+            case 'CredentialsSignin':
+                return 'Invalid credentials.';
+            default:
+                return 'Something went wrong.';
             }
         }
         throw error;
